1efb3439b03f7a616a035fdc1502c1acf4a6ead3
Thant Zin Aung <tza.personal.dev@gmail.com>
2025-05-18 22:32:12 +0630

    Create TaskForm component and added new basic style

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -3,17 +3,39 @@ import styled from 'styled-components';
 const NewTaskFormContainer = styled.div`
     width: 100vw;
     height: 100vh;
-    background-color: rgba(0, 0, 0, 0.7);
+    background-color: rgba(0, 0, 0, 0.8);
     position: absolute;
     top: 0;
     left: 0;
-    z-index: 10000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: none;
+`;
+const TaskFormContainer = styled.div`
+    width: 600px;
+    height: max-content;
+    padding: 5px;
+    background-color: white;
+    border-radius: 15px;
+`;
+const TaskForm = styled.div`
+    width: 100%;
+    padding: 30px;
+    border: 2px solid;
+    border-color: #e9e9e9;
+    border-radius: 15px;
 `;
 
 export function NewTaskForm() {
     return (
         <NewTaskFormContainer>
-            <h1>Hello from form</h1>
+            <TaskFormContainer>
+                <TaskForm>
+                    <h1>Hello from form</h1>    
+                </TaskForm>    
+            </TaskFormContainer>
+            
         </NewTaskFormContainer>
     )
 }
